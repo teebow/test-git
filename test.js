7be0ccdde1e78617274e7eb3a35e54454a23e824
Thibaud <contact@teebow.net>
2018-04-11 16:10:20 +0200

    update test.js

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-export default (state = 'toto', action) => {
+export default (state = 'tata', action) => {
   switch (action.type) {
-    case 'f2':
-      console.log('toto');
+    case 'f1':
+      console.log('tata');
     case 'f4':
-      console.log('f4');  
+      console.log('f4');
     default:
       return state;
   }
