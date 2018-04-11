030bf5734740aacadd8c9382685505d227535b8b
Thibaud <contact@teebow.net>
2018-04-11 14:40:25 +0200

    add afunc

---
+++ test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import { connect } from 'react-redux';
+class toto extends Component {
+    state = { a: false };
+
+    afunc = () => console.log('function from a');
+
+    render() {
+        return (
+            'tata';
+        );
+    }
+}
+
+export default toto;
