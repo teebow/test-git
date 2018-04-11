416463a94e04261ef782cf68845f192cd657042d
Thibaud <contact@teebow.net>
2018-04-11 15:54:20 +0200

    add f4.js and update test.js

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ export default (state = 'toto', action) => {
   switch (action.type) {
     case 'f2':
       console.log('toto');
+    case 'f4':
+      console.log('f4');  
     default:
       return state;
   }
